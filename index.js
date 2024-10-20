390589bcf67f7d30f7a8c0870899cae5c43c88ca
Sandeep <sandeep.patel4488@gmail.com>
2024-10-20 17:27:22 +0530

    User, admin and course routes added. Sigin and Signup functionality added

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+console.log(process.env.MONGO_URL);
+
+const express = require('express')
+const mongoose = require("mongoose");
+
+const { userRouter } = require('./routes/user');
+const { adminRouter } = require('./routes/admin');
+const { courseRouter } = require('./routes/course');
+
+const app = express()
+app.use(express.json);
+
+const port = 3000
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/course", courseRouter);
+
+async function main(){
+  await mongoose.connect(process.env.MONGO_URL);
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  });
+};
+
+main();
