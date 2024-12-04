ba2ca4809165af6fc501e7710ed81e9defa36621
Sandeep <sandeep.patel4488@gmail.com>
2024-12-04 20:28:37 +0530

    SignIn, addCourse, updateCourse converted to Typescript

--- dist/app/controllers/adminController.js
+++ dist/app/controllers/adminController.js
@@ -12,16 +12,26 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const zod_1 = require("zod");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { ObjectId } = require('mongoose').Types;
+// const { ObjectId } = require('mongoose').Types;
 const { JWT_ADMIN_PASSWORD } = require("../../config");
 const admin_1 = require("../../database/models/admin");
 const { courseModel } = require("../../database/models/course");
-const { courseSchema } = require("../../database/schema/courseSchema");
+const mongoose_1 = require("mongoose");
 const signUpSchema = zod_1.z.object({
-    email: zod_1.z.string().email(), // Must be a valid email
-    password: zod_1.z.string().min(6), // Minimum password length of 6 characters
-    firstName: zod_1.z.string().min(1, "First name is required"), // First name must not be empty
+    email: zod_1.z.string().email(),
+    password: zod_1.z.string().min(6),
+    firstName: zod_1.z.string().min(1, "First name is required"),
     lastName: zod_1.z.string().min(1) // Last name must not be empty
+});
+const signInSchema = zod_1.z.object({
+    email: zod_1.z.string().email(),
+    password: zod_1.z.string().min(6) // Minimum password length of 6 characters
+});
+const courseSchema = zod_1.z.object({
+    title: zod_1.z.string().min(1),
+    description: zod_1.z.string().min(1),
+    price: zod_1.z.number().min(1),
+    imageUrl: zod_1.z.string().min(1)
 });
 function signUp(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
@@ -65,6 +75,130 @@ function signUp(req, res) {
         }
     });
 }
+function signIn(req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const signData = signInSchema.parse(req.body);
+            const admin = yield admin_1.adminModel.findOne({ email: signData.email });
+            if (!admin) {
+                return res.status(400).json({
+                    message: "Admin with no such email exists !"
+                });
+            }
+            const isMatch = yield bcrypt.compare(signData.password.trim(), admin.password);
+            if (!isMatch) {
+                return res.status(401).json({
+                    "message": "Invalid email or password"
+                });
+            }
+            const token = jwt.sign({
+                id: admin._id,
+                role: "admin"
+            }, JWT_ADMIN_PASSWORD, { expiresIn: '1h' });
+            res.json({
+                message: 'Login successful',
+                token: token
+            });
+        }
+        catch (error) {
+            if (error instanceof zod_1.z.ZodError) {
+                return res.status(400).json({
+                    message: 'Validation Error',
+                    errors: error.errors
+                });
+            }
+            res.status(500).json({
+                message: "Error during login",
+                error: error instanceof Error ? error.message : "Unknown Error"
+            });
+        }
+    });
+}
+function addCourse(req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            if (!req.user || !req.user.id) {
+                res.status(401).json({ message: "Unauthorized: User ID is missing" });
+                return;
+            }
+            const creatorId = req.user.id;
+            const courseData = courseSchema.parse(req.body);
+            const newCourse = yield courseModel.create(Object.assign(Object.assign({}, courseData), { creatorId: creatorId }));
+            res.status(201).json({
+                message: "Course created successfully !",
+                course: newCourse
+            });
+        }
+        catch (error) {
+            if (error instanceof zod_1.z.ZodError) {
+                return res.status(400).json({
+                    error: error.errors,
+                    message: "Validation Error"
+                });
+            }
+            res.status(500).json({
+                error: error instanceof Error ? error.message : "Unknown Error",
+                message: "Error during saving a course !"
+            });
+        }
+    });
+}
+function updateCourse(req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const { courseId } = req.params;
+        if (!mongoose_1.Types.ObjectId.isValid(courseId)) {
+            return res.status(400).json({ message: 'Invalid Course ID' });
+        }
+        const courseUpdateSchema = zod_1.z.object({
+            title: zod_1.z.string().min(1),
+            description: zod_1.z.string().min(1),
+            price: zod_1.z.string()
+                .transform((val) => Number(val)) // Convert string to number
+                .refine((val) => !isNaN(val) && val >= 1, { message: "Price must be a number and at least 1" }),
+            imageUrl: zod_1.z.string().min(1)
+        });
+        try {
+            if (!req.user || !req.user.id) {
+                res.status(401).json({ message: "Unauthorized: User ID is missing" });
+                return;
+            }
+            const courseData = courseUpdateSchema.parse(req.body);
+            const course = yield courseModel.findById(courseId);
+            if (!course) {
+                return res.status(404).json({ message: 'Course not found' });
+            }
+            // Compare userId from JWT with creatorId of the course
+            if (!course.creatorId || course.creatorId.toString() !== req.user.id) {
+                return res.status(403).json({ message: 'Unauthorized to update this course' });
+            }
+            const updatedCourse = yield courseModel.findByIdAndUpdate(courseId, {
+                $set: {
+                    title: courseData.title,
+                    description: courseData.description,
+                    price: courseData.price,
+                    imageUrl: courseData.imageUrl
+                }
+            }, { new: true } // Return the updated document
+            );
+            res.status(200).json({
+                message: "Course updated successfully !",
+                course: updatedCourse
+            });
+        }
+        catch (error) {
+            if (error instanceof zod_1.z.ZodError) {
+                return res.status(400).json({
+                    error: error.errors,
+                    message: "Validation Error"
+                });
+            }
+            res.status(500).json({
+                error: error instanceof Error ? error.message : "Unknown Error",
+                message: "Error during updating a course !"
+            });
+        }
+    });
+}
 function list(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
@@ -98,4 +232,4 @@ function list(req, res) {
 //     signUp: signUp,
 //     list: list
 // };
-exports.default = { list, signUp };
+exports.default = { list, signUp, signIn };
--- src/app/controllers/adminController.ts
+++ src/app/controllers/adminController.ts
@@ -1,14 +1,15 @@
 import { z } from "zod";
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { ObjectId } = require('mongoose').Types;
+// const { ObjectId } = require('mongoose').Types;
 const { JWT_ADMIN_PASSWORD } = require("../../config");
 import { adminModel, AdminDocument } from "../../database/models/admin";
 const { courseModel } = require("../../database/models/course");
-const { courseSchema } = require("../../database/schema/courseSchema");
+// const { courseSchema } = require("../../database/schema/courseSchema");
 // const { signUpSchema, signInSchema } = require("../../database/schema/userSchema");
 
 import { Request, Response } from "express"
+import { Types } from "mongoose";
 
 declare global {
     namespace Express {
@@ -40,8 +41,28 @@ const signUpSchema = z.object({
     lastName: z.string().min(1)             // Last name must not be empty
 });
 
+const signInSchema = z.object({
+    email: z.string().email(),              // Must be a valid email
+    password: z.string().min(6)            // Minimum password length of 6 characters
+});
+
+const courseSchema = z.object({
+    title: z.string().min(1),
+    description: z.string().min(1),
+    price: z.number().min(1),
+    imageUrl: z.string().min(1)
+});
+
 // Define the TypeScript type for the request body based on the Zod schema
 type SignUpData = z.infer<typeof signUpSchema>;
+type SignData = z.infer<typeof signInSchema>;
+type CourseData = {
+    title: string;
+    description: string;
+    price: number;
+    imageUrl: string;
+    creatorId?: string;
+};
 
 async function signUp(req: Request, res: Response) {
     try {
@@ -92,6 +113,156 @@ async function signUp(req: Request, res: Response) {
     }
 }
 
+async function signIn(req: Request, res: Response) {
+    try {
+        const signData: SignData = signInSchema.parse(req.body);
+
+        const admin: AdminDocument | null = await adminModel.findOne({ email: signData.email });
+        if (!admin) {
+            return res.status(400).json({
+                message: "Admin with no such email exists !"
+            })
+        }
+
+        const isMatch: boolean = await bcrypt.compare(signData.password.trim(), admin.password);
+        if (!isMatch) {
+            return res.status(401).json({
+                "message": "Invalid email or password"
+            });
+        }
+
+        const token: string = jwt.sign(
+            {
+                id: admin._id,
+                role: "admin"
+            },
+            JWT_ADMIN_PASSWORD,
+            { expiresIn: '1h' }
+        );
+
+        res.json({
+            message: 'Login successful',
+            token: token
+        })
+    }
+    catch (error: unknown) {
+        if (error instanceof z.ZodError) {
+            return res.status(400).json({
+                message: 'Validation Error',
+                errors: error.errors
+            });
+        }
+
+        res.status(500).json({
+            message: "Error during login",
+            error: error instanceof Error ? error.message : "Unknown Error"
+        })
+    }
+}
+
+async function addCourse(req: Request, res: Response) {
+    try {
+        if (!req.user || !req.user.id) {
+            res.status(401).json({ message: "Unauthorized: User ID is missing" });
+            return;
+        }
+
+        const creatorId: string = req.user.id;
+
+        const courseData: CourseData = courseSchema.parse(req.body);
+
+        const newCourse: CourseData = await courseModel.create({
+            ...courseData,
+            creatorId: creatorId
+        });
+
+        res.status(201).json({
+            message: "Course created successfully !",
+            course: newCourse
+        });
+    }
+    catch (error: unknown) {
+        if (error instanceof z.ZodError) {
+            return res.status(400).json({
+                error: error.errors,
+                message: "Validation Error"
+            });
+        }
+
+        res.status(500).json({
+            error: error instanceof Error ? error.message : "Unknown Error",
+            message: "Error during saving a course !"
+        });
+    }
+}
+
+async function updateCourse(req: Request, res: Response) {
+    const { courseId } = req.params;
+
+    if (!Types.ObjectId.isValid(courseId)) {
+        return res.status(400).json({ message: 'Invalid Course ID' });
+    }
+
+    const courseUpdateSchema = z.object({
+        title: z.string().min(1),
+        description: z.string().min(1),
+        price: z.string()
+            .transform((val) => Number(val)) // Convert string to number
+            .refine((val) => !isNaN(val) && val >= 1, { message: "Price must be a number and at least 1" }),
+        imageUrl: z.string().min(1)
+    });
+
+    try {
+        if(!req.user || !req.user.id){
+            res.status(401).json({ message: "Unauthorized: User ID is missing" });
+            return; 
+        }
+        const courseData: CourseData = courseUpdateSchema.parse(req.body);
+
+        const course: CourseData = await courseModel.findById(courseId);
+
+        if (!course) {
+            return res.status(404).json({ message: 'Course not found' });
+        }
+
+        // Compare userId from JWT with creatorId of the course
+        if (!course.creatorId || course.creatorId.toString() !== req.user.id) {
+            return res.status(403).json({ message: 'Unauthorized to update this course' });
+        }
+
+        const updatedCourse: CourseData = await courseModel.findByIdAndUpdate(
+            courseId,
+            {
+                $set: {
+                    title: courseData.title,
+                    description: courseData.description,
+                    price: courseData.price,
+                    imageUrl: courseData.imageUrl
+                }
+            },
+            { new: true }  // Return the updated document
+        )
+
+        res.status(200).json({
+            message: "Course updated successfully !",
+            course: updatedCourse
+        })
+    }
+    catch (error: unknown) {
+        if (error instanceof z.ZodError) {
+            return res.status(400).json({
+                error: error.errors,
+                message: "Validation Error"
+            });
+        }
+
+        res.status(500).json({
+            error: error instanceof Error ? error.message : "Unknown Error",
+            message: "Error during updating a course !"
+        })
+    }
+}
+
 async function list(req: Request, res: Response) {
     try {
         if (!req.user || req.user.id) {
@@ -127,4 +298,4 @@ async function list(req: Request, res: Response) {
 //     list: list
 // };
 
-export default { list, signUp };
+export default { list, signUp, signIn };
