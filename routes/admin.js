00d8e7a7fa7870dd3e3a4f200633dd1723438222
Sandeep Patel <sandeep.patel4488@gmail.com>
2024-12-12 17:45:29 +0530

    Prisma CRUD for user

--- routes/admin.js
+++ routes/admin.js
@@ -18,7 +18,7 @@ adminRouter.delete("/course/:courseId", adminMiddleware, adminController.deleteC
 
 adminRouter.get("/list", adminMiddleware, adminController.list);
 
-adminRouter.post("/test/new/user", async function(req, res) {
+adminRouter.post("/test/new/user", async function (req, res) {
     const user = await prisma.newUser.create({
         data: {
             email: 'sandeep@example.com',
@@ -28,7 +28,88 @@ adminRouter.post("/test/new/user", async function(req, res) {
         },
     });
     console.log(user);
-    return res.json({message: "User added successfuly !"});
+    return res.json({ message: "User added successfuly !" });
+});
+
+adminRouter.get("/test/new/user/:userId", async function (req, res) {
+    try {
+        const { userId } = req.params;
+        console.log(userId);
+        const user = prisma.newUser.findUnique({
+            where: { id: userId },
+        })
+        return res.json({
+            users: user
+        })
+    }
+    catch (error) {
+        console.error(error.message);
+    }
+});
+
+adminRouter.get("/test/new/user/", async function (req, res) {
+    try {
+        const users = await prisma.newUser.findMany();
+        return res.json({
+            users: users
+        })
+    }
+    catch (error) {
+        console.error(error.message);
+    }
+});
+adminRouter.post("/test/new/user/update/:userId", async function (req, res) {
+    try {
+        const { userId } = req.params;
+        console.log(userId);
+        const user = prisma.newUser.findUnique({
+            where: { _id: userId },
+        })
+        if (!user) {
+            return res.status(404).json({
+                message: "no user found to update !"
+            })
+        }
+
+        const { email, password, firstName, lastName } = req.body;
+
+        const udatedUser = prisma.newUser.update({
+            where: { _id: userId },
+            email,
+            password,
+            firstName,
+            lastName,
+        });
+
+        return res.json({
+            udatedUser
+        })
+    }
+    catch (error) {
+        console.error(error.message);
+    }
+});
+adminRouter.post("/test/new/user/delete/:userId", async function (req, res) {
+    try {
+        const { userId } = req.params;
+        const user = prisma.newUser.findUnique({
+            where: { _id: userId },
+        })
+        if (!user) {
+            return res.status(404).json({
+                message: "no user found !"
+            })
+        }
+        prisma.newUser.delete({
+            where: { _id: userId },
+        })
+        return res.json({
+            users: []
+        })
+    }
+    catch (error) {
+        console.error(error.message);
+    }
 });
 
 module.exports = {
