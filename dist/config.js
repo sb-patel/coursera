d2a2a49de2d2207e6b3e13f666b60420ee1a3736
Sandeep Patel <sandeep.patel4488@gmail.com>
2024-12-30 14:10:00 +0530

    Remove dist folder from version control

--- dist/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JWT_ADMIN_PASSWORD = exports.JWT_USER_PASSWORD = void 0;
-exports.JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
-exports.JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
